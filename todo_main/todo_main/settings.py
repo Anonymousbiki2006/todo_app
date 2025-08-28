4236bb04180821ec4452e74e35ce41ed65364ff9
Bikram Das <73586092+Anonymousbiki2006@users.noreply.github.com>
2025-08-28 17:18:30 +0000

    csrf trusted origin

--- todo_main/todo_main/settings.py
+++ todo_main/todo_main/settings.py
@@ -130,3 +130,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://todo-app-4p6p.onrender.com",
+    "https://www.todo-app-4p6p.onrender.com"
+]
